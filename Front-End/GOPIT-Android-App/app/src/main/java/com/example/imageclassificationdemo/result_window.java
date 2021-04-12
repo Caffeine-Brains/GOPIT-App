b1308cc70c3dfa4771f0de982a1e1139a89d4242
Chathurangani Ranathunga <66179809+ChathuranganiRanathunga@users.noreply.github.com>
2021-04-12 20:49:10 +0530

    Add Data set part to document and create result window UI
    Result window UI is not finished

---
+++ Front-End/GOPIT-Android-App/app/src/main/java/com/example/imageclassificationdemo/result_window.java
@@ -0,0 +1,19 @@
+package com.example.imageclassificationdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class result_window extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_window);
+
+    }
+}
