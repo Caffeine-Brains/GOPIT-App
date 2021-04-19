d89b829db02d036204d1e7b6488c5310343ca338
Dinithi Hewawasam <72222128+Dinithi163@users.noreply.github.com>
2021-04-19 22:27:21 +0530

    Add Bottom Navigation to aboutus

--- Front-End/GOPIT-Android-App/app/src/main/java/com/example/imageclassificationdemo/AboutUs.java
+++ Front-End/GOPIT-Android-App/app/src/main/java/com/example/imageclassificationdemo/AboutUs.java
@@ -9,11 +9,56 @@ import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 public class AboutUs extends AppCompatActivity {
 
+    //Bottom Navigation
+    private MeowBottomNavigation bottomNavigation;
+    private final int ID_HOME = 1;
+    private final int ID_REAL = 2;
+    private final int ID_HELP = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        //Bottom Navigation
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_HOME,R.drawable.ic_baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_REAL,R.drawable.ic_baseline_check_circle_outline_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_HELP,R.drawable.ic_baseline_help_outline_24));
+
+        //Bottom Navigation Activities
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+
+            }
+        });
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+
+            }
+        });
+
+        //Bottom Navigation
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                switch (item.getId()) {
+                    case ID_HOME:
+                        startActivity(new Intent(AboutUs.this, selectOptionActivity.class));
+                        break;
+                    case ID_REAL:
+                        startActivity(new Intent(AboutUs.this, ScanActivity.class));
+                        break;
+                    case ID_HELP:
+                        startActivity(new Intent(AboutUs.this, Help.class));
+                        break;
+                }
+            }
+        });
 
     }
 }
